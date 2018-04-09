e0a24543ca0c4388e1b885e9059ad75579900ed2
Marcus Medom Ryding <mrok4a@gmail.com>
2018-04-09 20:44:56 +0200

    Slightly improve the classification ANN maybe

--- ann_classification.py
+++ ann_classification.py
@@ -13,11 +13,11 @@ def train(data_x, data_y):
     model.add(Dense(units=100, activation='tanh'))
     model.add(Dense(units=2, activation='softmax'))
 
-    model.compile(loss=keras.losses.kullback_leibler_divergence, optimizer='adamax', metrics=['acc'])
+    model.compile(loss=keras.losses.kullback_leibler_divergence, optimizer='adadelta', metrics=['acc'])
 
     model.fit(data_x, data_y,
-              validation_split=0.3, epochs=1000, batch_size=1000,
-              callbacks = [EarlyStopping(monitor='val_acc', patience=100)],
+              validation_split=0.3, epochs=1000, batch_size=10,
+              #callbacks = [EarlyStopping(monitor='val_acc', patience=10)],
               verbose = 2)
 
     def predict(data):
